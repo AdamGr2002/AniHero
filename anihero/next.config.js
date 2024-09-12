645c53fc29147e7f1fe428823cb069679eaa9f59
adam <adam.guerchi@tek-up.de>
2024-09-12 22:57:31 +0100

    Update Content-Security-Policy in next.config.js

--- anihero/next.config.js
+++ anihero/next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     images: {
       domains: ['cdn.myanimelist.net'],
     },
-    experimental: {
-      serverActions: true,
-    },
     async headers() {
       return [
         {
@@ -14,7 +11,7 @@ const nextConfig = {
           headers: [
             {
               key: 'Content-Security-Policy',
-              value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://clerk.anihero.vercel.app; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://clerk.anihero.vercel.app https://api.jikan.moe;",
+              value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://clerk.anihero.vercel.app https://*.clerk.accounts.dev; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://clerk.anihero.vercel.app https://api.jikan.moe https://*.clerk.accounts.dev;",
             },
           ],
         },
